9091ee754d2a3915f990a4e1f5b2b422cd0d30ef
Zsvob <svobodazachary@gmail.com>
2022-05-19 11:04:25 -0400

    Primitive Operations HW1

--- PrimitiveOperations.java
+++ PrimitiveOperations.java
@@ -5,6 +5,14 @@ public class PrimitiveOperations {
         System.out.println(awesome);
         System.out.println(sweet);
         double producto = (awesome * sweet);
-        System.out.println(producto)
+        System.out.println(producto);
+        double casty = (awesome);
+        System.out.println(casty);
+        int casty2 = (int)sweet;
+        System.out.println(casty2);
+        char char1='G';
+        System.out.println(char1);
+        int char2 = (int)char1 + 32;
+        System.out.println((char)char2);
     }
 }
