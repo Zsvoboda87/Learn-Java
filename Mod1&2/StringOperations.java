e2d3838159048fe769cd8d40d6b5f16402eb0854
Zsvob <svobodazachary@gmail.com>
2022-05-25 11:58:09 -0400

    assessment

--- Mod1&2/StringOperations.java
+++ Mod1&2/StringOperations.java
@@ -2,5 +2,14 @@ public class StringOperations {
     public static void main(String[] args){
         String me = "Zachary Svoboda";
         System.out.println(me);
-    }
+        String newme = me.replace('Z', 'A');
+        System.out.println(newme);
+        String newme2 = newme.substring(0, 14).concat("z");
+        System.out.println(newme2);        
+        String url = "www.zack.com";
+        System.out.println(url);
+        String url2 = url.substring(4, 8);
+        String url3 = url2.concat("1331");
+        System.out.println(url3);
+}
 }
