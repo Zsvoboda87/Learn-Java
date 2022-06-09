776b52271d6a2a176705605b01cb4abc63b81f42
Zsvob <svobodazachary@gmail.com>
2022-06-08 20:47:28 -0400

    calculator

--- Mod 3/Calculator.java
+++ Mod 3/Calculator.java
@@ -1,3 +1,47 @@
+import java.util.Scanner; 
+
 public class Calculator {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an operation: (add, substract, multiply, divide, or alphabetize)" );
+        String op = input.next();
+        op.toLowerCase();
+
+        // if( op.equals("alphabetize")) {
+           
+        // }
+
+
+        if( op.equals("add")) {
+            System.out.println("Enter two integers:" );
+            int a, b;
+            a = input.nextInt();
+            b = input.nextInt();
+            System.out.println(a + b );
+        } else if (op.equals("subtract")){
+            System.out.println("Enter two integers:" );
+            int a, b;
+            a = input.nextInt();
+            b = input.nextInt();
+            System.out.println(a - b );
+        } else if (op.equals("multiply")){
+            System.out.println("Enter two doubles:" );
+            double a, b;
+            a = input.nextInt();
+            b = input.nextInt();
+            System.out.println(a * b );
+        } else if (op.equals("divide")){
+            System.out.println("Enter two doubles:" );
+            double a, b;
+            a = input.nextInt();
+            b = input.nextInt();
+            if(b == 0) {
+                System.out.println("Invalid input entered. Terminating...");
+            } else {
+            System.out.println(a / b );}
+        } else {
+            System.out.println("Invalid input entered. Terminating...");
+        }
+
+    }
 }
--- Mod 3/Powers.java
+++ Mod 3/Powers.java
@@ -1,3 +1,5 @@
+import java.util.Scanner
+
 public class Powers {
     public static void main(String[] args) {
         final int LIMIT =40;
